b47ac377d658f3e4c776d5aa5c9225ff3a6bdf81
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2024-05-27 14:55:38 +0200

    Adding a new .csv file

--- PlottingJavaMemory/main.py
+++ PlottingJavaMemory/main.py
@@ -15,12 +15,16 @@ def main():
     dataframe = pd.read_csv('CSVresources/Speicher_Java_LinuxMint.csv')
     memoryLinuxMint = dataframe.iloc[:-1, 1] # Verwende nicht den letzten Wert, da er negativ sein kann!
 
+    dataframe = pd.read_csv('CSVresources/Speicher_Java_Pi.csv')
+    memoryPi = dataframe.iloc[:-1, 1]
+
     dataframe = pd.read_csv('CSVresources/Speicher_Java_Windows.csv')
     memoryWindows = dataframe.iloc[:-1, 1]
 
 
     #plot the data
     plot_data(memoryWindows/10**6, 'Speicherverbrauch im Heap (Windows)')
+    plot_data(memoryPi/10**6, 'Speicherverbrauch im Heap (Raspberry Pi)')
     plot_data(memoryLinuxMint/10**6, 'Speicherverbrauch im Heap (Linux Mint)')
 
 
--- PythonFourier/README.txt
+++ PythonFourier/README.txt
@@ -10,3 +10,9 @@ Starten des Programms:
 
 
 python main.py ..\resources\nicht_zu_laut_abspielen_sehr_kurz.wav 1024
+
+
+
+
+Falls Sie die Fehlermeldung "ImportError: libopenblas.so.0: cannot open shared object file or directory" bekommen, dann muessen sie diese Bibliothek installieren:
+sudo apt-get install libopenblas-dev
