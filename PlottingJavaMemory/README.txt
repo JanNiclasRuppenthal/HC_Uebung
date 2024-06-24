33c3b9486ee2d703046a31e61f285832e0309215
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2024-06-24 21:45:10 +0200

    Sequentiell FFT implementation
    This implementation reads four parameters and writes all frequencies with amplitudes higher than a threshold.

--- PlottingJavaMemory/README.txt
+++
@@ -1,8 +0,0 @@
-Bevor das Skript main.py gestartet werden kann, muss man zuerst alle Bibliotheken herunterladen. Hierfuer kann man den folgenden Befehl verwenden:
-pip install -r requirements.txt
-
-
-Um dieses Programm zu starten, verwenden Sie den folgenden Befehl:
-python main.py oder python3 main.py 
-
-Die CSV-Dateien wurden aus der JConsole exportiert.
--- PythonFourier/main.py
+++ PythonFourier/main.py
@@ -1,4 +1,3 @@
-import cmath
 import sys
 import time
 
@@ -6,29 +5,25 @@ import numpy as np
 
 from scipy.io import wavfile
 
-# Aufgabe 2
-import tracemalloc
-
 
 def get_all_arguments():
     file_path = str(sys.argv[1])
     block_size = int(sys.argv[2])
 
-    algorithm_option = str(sys.argv[3]) if (len(sys.argv) > 3) else None
-    if algorithm_option == 'rec_fft':
-        func = fft
-    elif algorithm_option == 'vec_fft':
-        func = fft_vectorized
+    if (block_size < 64):
+        block_size = 64
+    elif (block_size > 512):
+        block_size = 512
 
-        if np.log2(block_size) % 1 > 0:
-            raise ValueError("Die Groesse des Samples muss eine Potenz von 2 sein, da sonst der Algorihtmus nicht funktioniert.")
+    shift_size = int(sys.argv[3])
+    if (shift_size < 1):
+        shift_size = 1
+    elif (shift_size > block_size):
+        shift_size = block_size
 
-    elif algorithm_option == 'dft':
-        func = dft
-    elif algorithm_option is None or algorithm_option == "np_fft":
-        func = np.fft.fft 
+    threshold = int(sys.argv[4])
 
-    return file_path, block_size, func
+    return file_path, block_size, shift_size, threshold
 
 
 def analyze_wav_file(file_path):
@@ -43,80 +38,17 @@ def analyze_wav_file(file_path):
 
     return data, sample_rate
 
-'''
-Diese Methode wurde von ChatGPT generiert.
-Ich habe die Methode nicht wirklich getestet, da die DFT sehr viel Rechenzeit benoetigt.
-'''
-def dft(data_block):
-    N = len(data_block)
-    X = np.zeros(N, dtype=complex)
-    for k in range(N):
-        for n in range(N):
-            X[k] += data_block[n] * cmath.exp(-2j * np.pi * k * n / N)
-    return X
 
-
-'''
-Die FFT Implementierung habe ich von der folgenden Webseite:
-https://jakevdp.github.io/blog/2013/08/28/understanding-the-fft/
-
-Die Webseite ist sehr interessant, da sie verschiedene Implementierungen miteinander vergleicht.
-Ausserdem zeigt der Autor, dass seine DFT Implementierung 1000-mal langsamer ist, als die Implementierung von numpy.
-
-Ich habe den Code ein wenig angepasst.
-'''
-def fft_vectorized(data_block):
-    data_block = np.asarray(data_block, dtype=float)
-    block_size = len(data_block)
-
-    N_min = min(block_size, 32)
-
-    # Berechne DFT auf ein Unterproblem mit einer Groesse von N_min <= 32
-    n = np.arange(N_min)
-    k = n[:, None]
-    M = np.exp(-2j * np.pi * n * k / N_min)
-    X = np.dot(M, data_block.reshape((N_min, -1)))
-
-    # Berechne FFT iterativ, anstatt rekursiv
-    while X.shape[0] < block_size:
-        X_even = X[:, :X.shape[1]//2]
-        X_odd = X[:, X.shape[1]//2:]
-        factor = np.exp(-1j * np.pi * np.arange(X.shape[0])
-                        / X.shape[0])[:, None]
-        X = np.vstack([X_even + factor * X_odd,
-                       X_even - factor * X_odd])
-
-    return X.ravel()
-
-
-def fft(x):
-    N = len(x)
-    if N <= 1:
-        return x
-    even = fft(x[::2])
-    odd = fft(x[1::2])
-    factor = np.exp(-2j * np.pi * np.arange(N) / N)
-    return np.concatenate([even + factor[:N//2] * odd,
-                           even + factor[N//2:] * odd])
-
-'''
-Der fuer die Aufgabe 01 eigentliche Analyse Algorithmus
-'''
-def analyze(data, block_size, fourier_function):
+def analyze(data, block_size, shift_size):
     num_samples = len(data)
     num_blocks = num_samples - block_size + 1
 
     aggregated_fft = np.zeros(block_size//2) #Redundanz der Spiegelung entfernen
 
-    current, peak = tracemalloc.get_traced_memory()
-    currentDataMB = [current/10**6]
-    peakDataMB = [peak/10**6]
-
     # Fuer jeden Datenblock wird die jeweilige ausgewaehlte Funktion angewendet.
-    for i in range(num_blocks):
-        tracemalloc.reset_peak()
+    for i in range(0, num_blocks, shift_size):
         block = data[i:i+block_size]
-        fft_result = fourier_function(block)
+        fft_result = np.fft.fft(block)
 
         '''
         Summiere alle Ergebnisse auf.
@@ -125,19 +57,8 @@ def analyze(data, block_size, fourier_function):
         '''
         aggregated_fft += np.abs(fft_result[:block_size//2])
 
-        current, peak = tracemalloc.get_traced_memory()
-        currentDataMB.append(current/10**6)
-        peakDataMB.append(peak/10**6)
-
-
     # Wir berechnen den Mittelwert, da die Summen sonst zu gross sind
     aggregated_fft /= num_blocks
-
-    current, peak = tracemalloc.get_traced_memory()
-    currentDataMB.append(current/10**6)
-    peakDataMB.append(peak/10**6)
-    write_data_to_file(currentDataMB, "currentMB.txt")
-    write_data_to_file(peakDataMB, "peakMB.txt")
 
     return aggregated_fft
 
@@ -158,26 +79,19 @@ def print_run_time(run_time):
     print('Laufzeit: {} Minuten und {:.2f} Sekunden'.format(minutes, seconds))
 
 def main():
-    '''
-    Die Zeit wird nur als Zusatz gemessen.
-    Meines Erachtens sollte man auch neben dem Speicher immer die Zeit betrachten.
-    '''
     start_time = time.time()
-    tracemalloc.start()
 
-    file_path, block_size, fourier_function = get_all_arguments()
+    file_path, block_size, shift_size, threshold = get_all_arguments()
     wav_data, sample_rate = analyze_wav_file(file_path)
 
-    current, peak = tracemalloc.get_traced_memory()
-    print(f"Before Fourier Analysis: Memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
-    aggregated_fft = analyze(wav_data, block_size, fourier_function)
-
-    current, peak = tracemalloc.get_traced_memory()
-    print(f"After Fourier Analysis: Memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
-    tracemalloc.stop()
+    aggregated_fft = analyze(wav_data, block_size, shift_size)
 
     write_data_to_file([sample_rate, block_size], 'sample_rate_and_block_size.txt')
     write_data_to_file(aggregated_fft, 'aggregated_fft.txt')
+
+    result = [(index * sample_rate / block_size, aggregated_fft[index]) for index in range(len(aggregated_fft)) if aggregated_fft[index] > threshold]
+
+    print(result)
 
     run_time = time.time() - start_time
     print_run_time(run_time)
