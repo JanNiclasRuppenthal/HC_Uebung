0106012f248b39843c4b4e4a41802d4f2459ceff
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2024-07-28 18:14:03 +0200

    show the data on the ePaper display

--- main.py
+++ main.py
@@ -30,8 +30,18 @@ def setup_display(e_display):
     date_str = "{:02d}.{:02d}.{:d}".format(date[2], date[1], date[0])
     e_display.text(date_str, 210, 10, 0x00)
     
+    # temperature
+    temp_str = "Temperatur: "
+    e_display.text(temp_str, 5, 50, 0x00)
+    e_display.text("     Grad", 120, 50, 0x00)
+    
+    # humidity
+    hum_str = "Feuchtigkeit: "
+    e_display.text(hum_str, 5, 75, 0x00)
+    e_display.text("     %", 120, 75, 0x00)
+    
     # ip address
-    ip_str = "IP Address: X.X.X.X".format()
+    ip_str = "IP Adresse: X.X.X.X".format()
     e_display.text(ip_str, 5, 121, 0x00)
     
     # owner
@@ -52,6 +62,9 @@ def main():
     
     e_display = EPD_2in9_Landscape()
     setup_display(e_display)
+    last_temp = -1
+    last_humi = -1
+    change = False
     
     while True:
         try:
@@ -60,9 +73,29 @@ def main():
             temp = dht22_sensor.temperature()
             humi = dht22_sensor.humidity()
             
+            if (not last_temp == temp):
+                temp_str = "{:.1f}".format(temp)
+                e_display.fill_rect(120, 50, 35, 10, 0xff)
+                e_display.text(temp_str, 120, 50, 0x00)
+                
+                change = True
+                last_temp = temp
+                
+            if (not last_humi == humi):
+                humi_str = "{:.1f}".format(humi)
+                e_display.fill_rect(120, 75, 35, 10, 0xff)
+                e_display.text(humi_str, 120, 75, 0x00)
+                
+                change = True
+                last_humi = humi
+                
+            if (change):
+                e_display.display_Partial(e_display.buffer)
+                change = False
+            
             # print the measure stats to the terminal
-            print('Temperatur: {:.1f} °C'.format(temp))
-            print('Luftfeuchtigkeit: {:.1f} %'.format(humi))
+            print(temp)
+            print(humi)
         except Exception as e:
             print('Got an exception: ' + str(e))
         time.sleep(2)
