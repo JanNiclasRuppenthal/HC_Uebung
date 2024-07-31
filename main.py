dff03e8172d6c97787c7e878dd25b687f9100672
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2024-07-31 20:58:43 +0200

    Half integration of the server

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from machine import Pin, RTC
 import time
 from dht import DHT22
 from Pico_ePaper import EPD_2in9_Landscape
+from web_server import connect, open_socket, webpage
+import _thread
 
 weekday_str_list = [
     "Montag",
@@ -12,7 +14,6 @@ weekday_str_list = [
     "Samstag",
     "Sonntag"
 ]
-
 
 
 def setup_display(e_display):
@@ -41,13 +42,13 @@ def setup_display(e_display):
     e_display.text(hum_str, 5, 75, 0x00)
     e_display.text("     %", 120, 75, 0x00)
     
-    # ip address
-    ip_str = "IP Adresse: X.X.X.X".format()
+    # static ip address
+    ip_str = "IP Adresse: 192.168.178.114"
     e_display.text(ip_str, 5, 121, 0x00)
     
     # owner
     owner_str = "JNR#33"
-    e_display.text(owner_str, 210, 121, 0x00)
+    e_display.text(owner_str, 245, 121, 0x00)
     
     # lines
     e_display.hline(0, 20, 296, 0x00)
@@ -55,11 +56,40 @@ def setup_display(e_display):
     
     # show result on display
     e_display.display(e_display.buffer)
+    
+    
+temp = 0
+humi = 0
 
+
+def run_server():
+    global temp
+    global humi
+
+    try:
+        ip = connect()
+        connection = open_socket(ip)
+    except KeyboardInterrupt:
+        machine.reset()
+    
+    try:
+        while True:
+            client = connection.accept()[0]
+            html = webpage(temp, humi)
+            client.send(html)
+            client.close()
+    except Exception as e:
+        machine.reset()
     
 def main():
+    global temp
+    global humi
+    
+    #_thread.start_new_thread(run_server, ())
+    
     # initialize DHT22 sensor
     dht22_sensor = DHT22(Pin(0, Pin.IN, Pin.PULL_UP))
+    led = Pin("LED", Pin.OUT)
     
     e_display = EPD_2in9_Landscape()
     setup_display(e_display)
@@ -91,16 +121,15 @@ def main():
                 last_humi = humi
                 
             if (change):
-                #TODO: Blink the LED to indicate an update
+                led.on()
                 e_display.display_Partial(e_display.buffer)
                 change = False
+                led.off()
             
-            # print the measure stats to the terminal
-            #output_str = "Temperature: {:.1f} \nHumidity: {:.1f}".format(temp, humi)
-            #print(output_str)
         except Exception as e:
             print('Got an exception: ' + str(e))
-        time.sleep(2)
+        time.sleep(2)    
+    
 
 if __name__=='__main__':
     main()
---
+++ web_server.py
@@ -0,0 +1,100 @@
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor, pico_led
+import machine
+from wifi_configuration import ssid, password
+
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while not wlan.isconnected():
+        print('Waiting for a connection to the wifi')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected to wifi with the following ip: {ip}')
+    return ip
+    
+    
+def open_socket(ip):
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection
+    
+
+def webpage(temperature, humidity):
+    html = f"""
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wetterstation Raspberry Pi Pico W</title>
+    <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css">
+    <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+</head>
+<body style="background-color:#121212EE;">
+    <h1 style="text-align: center; color: orange">Temperatur</h1>
+    <div style="
+            border: 1px solid #000000;
+            background-color: aqua;
+            padding: 5%;
+            margin: 20px auto;
+            width: 50%;
+            text-align: center;
+            border-radius: 12px;
+            box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.3);
+            ">
+        <p style="font-size:200%"><b>{temperature}°C</b></p>
+    </div>
+    <h1 style="text-align: center; color: orange">Feuchtigkeit</h1>
+    <div style="
+            border: 1px solid #000000;
+            background-color: aqua;
+            padding: 5%;
+            margin: 20px auto;
+            width: 50%;
+            text-align: center;
+            border-radius: 12px;
+            box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.3);
+            ">
+        <p style="font-size:200%"><b>{humidity}%</b></p>
+    </div>
+    <div id="output" style="
+            padding: 20px;">
+    </div>
+    <py-script>
+        import micropip
+        import asyncio
+
+        async def main():
+            await micropip.install('matplotlib')
+            await micropip.install('numpy')
+            import matplotlib.pyplot as plt
+            import numpy as np
+
+            # Daten generieren
+            x = np.linspace(0, 10, 100)
+            y = np.sin(x)
+
+            # Plot erstellen
+            fig, ax = plt.subplots(figsize=(4, 4))
+            ax.plot(x, y)
+            ax.set(title='Sinusfunktion', xlabel='x', ylabel='sin(x)')
+
+            # Plot im HTML-Dokument anzeigen
+            from js import document
+            import pyscript
+
+            # Canvas-Element anpassen
+            pyscript.write('output', fig)
+
+        asyncio.ensure_future(main())
+    </py-script>
+</body>
+</html>
+            """
+    return str(html)
